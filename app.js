ab076a2c8d120c8da2299d4cf3686752af77c0cf
nayani <yapancs@gmail.com>
2024-12-18 13:01:10 -0500

    Initial Commit

--- app.js
+++ app.js
@@ -17,7 +17,13 @@ const morgan = require("morgan");
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: 'https://jobly-frontend-ickb.onrender.com',  // URL of your deployed frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],   // Allowed methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+  }
+));
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(authenticateJWT);
